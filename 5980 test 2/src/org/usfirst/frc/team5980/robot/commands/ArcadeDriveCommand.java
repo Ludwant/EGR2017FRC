3c5e5b8bb46206f56d38c177fb82b660253b110f
carliecouz <c.couzens5@gmail.com>
2017-01-19 20:53:05 -0500

    Added Arcade Drive

--- 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/ArcadeDriveCommand.java
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/ArcadeDriveCommand.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team5980.robot.commands;
 
 import org.usfirst.frc.team5980.robot.Robot;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -17,9 +18,26 @@ public class ArcadeDriveCommand extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     }
-
+    public double deadBand(double x){
+    	if (Math.abs(x)<.2){
+    		return 0;
+    	}else{
+    		return x;
+    	}
+    }
+    public double clip(double x){
+    	if (x>1) return 1;
+    	if (x<-1) return -1;
+    	return x;
+    }
+    
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double throttle = deadBand(-Robot.oi.driver.getY(Hand.kLeft));
+    	double wheel = deadBand(Robot.oi.driver.getX(Hand.kRight));
+    	double leftPower = clip (throttle + wheel);
+    	double rightPower = clip (throttle - wheel);
+    	Robot.driveTrain.setPower(leftPower, rightPower);
     }
 
     // Make this return true when this Command no longer needs to run execute()
