e438c2cdd57125546cf5ac7a206e5e1d3f2fe4f2
carliecouz <c.couzens5@gmail.com>
2017-01-24 15:52:40 -0500

    latest

--- 5980 test 2/src/org/usfirst/frc/team5980/robot/subsystems/DriveTrain.java
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/subsystems/DriveTrain.java
@@ -12,20 +12,20 @@ import com.ctre.CANTalon;
  *
  */
 public class DriveTrain extends Subsystem {
-	Victor left1, left2, right1, right2;
+	CANTalon left1, left2, right1, right2;
 	public DriveTrain() {
-		//right1 = new CANTalon(1);
-		//right2 = new CANTalon(2);
-		//left1 = new CANTalon(4);
-		//left2 = new CANTalon(3);
-		//right1.setInverted(true);
-		//right2.setInverted(true);
-		right1 = new Victor(2);
-		right2 = new Victor(3);
-		left1 = new Victor(0);
-		left2 = new Victor(1);
+		right1 = new CANTalon(1);
+		right2 = new CANTalon(2);
+		left1 = new CANTalon(4);
+		left2 = new CANTalon(3);
 		right1.setInverted(true);
 		right2.setInverted(true);
+		//right1 = new Victor(2);
+		//right2 = new Victor(3);
+		//left1 = new Victor(0);
+		//left2 = new Victor(1);
+		//right1.setInverted(true);
+		//right2.setInverted(true);
 	}
 	
 	public void setPower(double left, double right) {
