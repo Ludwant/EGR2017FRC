f3bd4af8e8105c9c9b82533687305100f19ee656
laure <laure@10.128.73.24>
2017-01-24 16:19:17 -0500

    Lauren's new drive train class

---
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/subsystems/TreadDriveTrain.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team5980.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import  edu.wpi.first.wpilibj.*;
+
+public class TreadDriveTrain extends Subsystem {
+	Victor right1, right2, left1, left2;
+	public TreadDriveTrain() {
+		right1 = new Victor(2);
+		right2 = new Victor(3);
+		left1 = new Victor(0);
+		left2 = new Victor(1);
+		right1.setInverted(true);
+		right2.setInverted(true);
+	}
+	public void setPower(double left, double right) {
+		left1.set(left);
+		left2.set(left);
+		right1.set(right);
+		right2.set(right);
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
