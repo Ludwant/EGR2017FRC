2b18e0df1a3b8804c94108b10c579e91460f3035
carliecouz <c.couzens5@gmail.com>
2017-01-27 12:05:52 -0500

    Changed GGC port

--- 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/GearGrabCommand.java
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/GearGrabCommand.java
@@ -20,7 +20,7 @@ public class GearGrabCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.gear.setPower(-Robot.oi.operator.getRawAxis(1));
+    	Robot.gear.setPower(-Robot.oi.operator.getRawAxis(5));
     }
 
     // Make this return true when this Command no longer needs to run execute()
