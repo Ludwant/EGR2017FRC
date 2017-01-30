3b7c1f8016c30a25597c6823fbfb7b882291356a
laure <laure@10.128.73.24>
2017-01-30 18:56:30 -0500

    hjh

--- 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/ArcadeDriveCommand.java
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/ArcadeDriveCommand.java
@@ -38,8 +38,8 @@ public class ArcadeDriveCommand extends Command {
     	double leftPower = clip (throttle + wheel);
     	double rightPower = clip (throttle - wheel);
     	Robot.driveTrain.setPower(leftPower, rightPower);
-    	SmartDashboard.putNumber("left encoder: ", Robot.sensors.getLeftEncoder());
-    	SmartDashboard.putNumber("left encoder: ", Robot.sensors.getRightEncoder());
+    	SmartDashboard.putNumber("Left Encoder: ", Robot.sensors.getLeftEncoder());
+    	SmartDashboard.putNumber("Right Encoder: ", Robot.sensors.getRightEncoder());
     }
 
     // Make this return true when this Command no longer needs to run execute()
