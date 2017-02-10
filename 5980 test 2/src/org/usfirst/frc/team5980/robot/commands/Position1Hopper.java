ae7e9053fed24863fbcf3294d68f6cc430c7794c
ccouz <ccouz@10.128.72.235>
2017-02-09 19:31:14 -0500

    Hopper Auto Commands

---
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/Position1Hopper.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team5980.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Position1Hopper extends CommandGroup {
+
+    public Position1Hopper() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+    	addSequential(new DriveBackwardsAutoCommand(.4, -500, 0));
+    	addSequential(new RotateToHeadingCommand(20, .25));
+    	addSequential(new DriveBackwardsAutoCommand(.4, -200, 20));
+    	addSequential(new RotateToHeadingCommand(0, .25));
+    	addSequential(new DriveBackwardsAutoCommand(.4, -1000, 0));
+    	
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
---
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/Position2Hopper.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team5980.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Position2Hopper extends CommandGroup {
+
+    public Position2Hopper() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+    	addSequential(new DriveBackwardsAutoCommand(.4, -250, 0));
+    	addSequential(new RotateToHeadingCommand(90, .25));
+    	addSequential(new DriveBackwardsAutoCommand(.4, -600, 90));
+    	addSequential(new RotateToHeadingCommand(-90, .25));
+    	addSequential(new DriveBackwardsAutoCommand(.4, -1000, 0));
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
