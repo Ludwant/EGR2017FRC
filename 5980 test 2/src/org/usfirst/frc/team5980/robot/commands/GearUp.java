2275020ab0fe28942a523dd4b94ed627cce57a3e
ccouz <ccouz@10.128.72.235>
2017-02-14 16:33:06 -0500

    gear commands

--- 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/GearUp.java
+++ 5980 test 2/src/org/usfirst/frc/team5980/robot/commands/GearUp.java
@@ -2,35 +2,30 @@ package org.usfirst.frc.team5980.robot.commands;
 
 import org.usfirst.frc.team5980.robot.Robot;
 
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.interfaces.Potentiometer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class GearDropCommand extends Command {
+public class GearUp extends Command {
 
-    public GearDropCommand() {
+    public GearUp() {
         // Use requires() here to declare subsystem dependencies
         requires(Robot.gear);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.gear.setPower(-1);
+    	Robot.gear.setPower(1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.gear.getUpperLimit();
+    	return Robot.gear.getUpperLimit();
     }
 
     // Called once after isFinished returns true
